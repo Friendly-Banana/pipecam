b95d426982c1daab3774e949734c28ab4b58d013
Banana <69007475+Just-Learned-It@users.noreply.github.com>
2021-09-06 13:36:14 +0200

    beispiel mit fehler

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Hello there")
+import cv2
+import mediapipe as mp
+
+config_text = """
+  input_stream: 'in_stream'
+  output_stream: 'out_stream'
+  node {
+    calculator: 'PassThroughCalculator'
+    input_stream: 'in_stream'
+    output_stream: 'out_stream'
+  }
+"""
+graph = mp.CalculatorGraph(graph_config=config_text)
+output_packets = []
+graph.observe_output_stream(
+    'out_stream',
+    lambda stream_name, packet:
+        output_packets.append(mp.packet_getter.get_str(packet)))
+
+graph.start_run()
+
+graph.add_packet_to_input_stream(
+    'in_stream', mp.packet_creator.create_str('abc').at(0))
+
+rgb_img = cv2.cvtColor(cv2.imread('/path/to/your/image.png'), cv2.COLOR_BGR2RGB)
+graph.add_packet_to_input_stream(
+    'in_stream',
+    mp.packet_creator.create_image_frame(format=mp.ImageFormat.SRGB,
+                                         data=rgb_img).at(1))
+
+graph.close()
