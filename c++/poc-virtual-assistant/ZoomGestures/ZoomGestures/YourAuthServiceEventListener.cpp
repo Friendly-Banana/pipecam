e26028cd375bd1b47866b63b53ca7a5035353254
Just-Learned-It <69007475+Just-Learned-It@users.noreply.github.com>
2021-09-10 11:39:11 +0200

    Delete c++ directory

--- c++/poc-virtual-assistant/ZoomGestures/ZoomGestures/YourAuthServiceEventListener.cpp
+++
@@ -1,8 +0,0 @@
-#include "YourAuthServiceEventListener.h"
-class YourAuthServiceEventListener :
-    public ZOOM_SDK_NAMESPACE::IAuthServiceEvent
-{
-    void onAuthenticationReturn(ZOOM_SDK_NAMESPACE::AuthResult ret) {
-        
-    }
-};
--- c++/poc-virtual-assistant/ZoomGestures/ZoomGestures/YourAuthServiceEventListener.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "C:\Users\c.frank\source\repos\ZoomGestures\h\auth_service_interface.h"
-
-
